96eb972dc90855313cc766b6f86bf679a7925760
Wen Tai-Chi <daleth90@gmail.com>
2022-07-20 22:32:05 +0800

    feat: calculate stats with modifiers

--- Runtime/StatOwner.cs
+++ Runtime/StatOwner.cs
@@ -5,11 +5,19 @@ namespace Physalia.Stats
 {
     public class StatOwner
     {
+        private class StatModifierSum
+        {
+            public int TotalAddend;
+            public int TotalMultiplier;
+        }
+
         private readonly int id;
         private readonly StatDefinitionTable table;
         private readonly StatOwnerRepository repository;
 
         private readonly Dictionary<int, Stat> stats = new();
+        private readonly List<StatModifier> modifiers = new();
+        private readonly Dictionary<int, StatModifierSum> modifierSumsCache = new();
 
         private bool isValid = true;
 
@@ -45,11 +53,13 @@ namespace Physalia.Stats
 
             var stat = new Stat(definition, originalValue);
             stats.Add(definition.Id, stat);
+            modifierSumsCache.Add(definition.Id, new StatModifierSum());
         }
 
         public void RemoveStat(int statId)
         {
             stats.Remove(statId);
+            modifierSumsCache.Remove(statId);
         }
 
         public Stat GetStat(int statId)
@@ -70,6 +80,55 @@ namespace Physalia.Stats
         public void ClearAllModifiers()
         {
             modifiers.Clear();
+        }
+
+        public void RefreshStats()
+        {
+            ResetAllStats();
+            for (var i = 0; i < modifiers.Count; i++)
+            {
+                var statId = modifiers[i].StatId;
+                if (!stats.ContainsKey(statId))
+                {
+                    continue;
+                }
+
+                var sum = modifierSumsCache[statId];
+                sum.TotalAddend += modifiers[i].Addend;
+                sum.TotalMultiplier += modifiers[i].Multiplier;
+            }
+
+            foreach (Stat stat in stats.Values)
+            {
+                var sum = modifierSumsCache[stat.Id];
+                if (sum.TotalMultiplier <= -100)
+                {
+                    stat.CurrentValue = 0;
+                }
+                else
+                {
+                    stat.CurrentValue = (int)((stat.CurrentValue + sum.TotalAddend) * (100 + sum.TotalMultiplier) / 100f);
+                }
+            }
+
+            ResetAllModifierSums();
+        }
+
+        private void ResetAllStats()
+        {
+            foreach (Stat stat in stats.Values)
+            {
+                stat.CurrentValue = stat.OriginalValue;
+            }
+        }
+
+        private void ResetAllModifierSums()
+        {
+            foreach (StatModifierSum sum in modifierSumsCache.Values)
+            {
+                sum.TotalAddend = 0;
+                sum.TotalMultiplier = 0;
+            }
         }
 
         public void Destroy()
