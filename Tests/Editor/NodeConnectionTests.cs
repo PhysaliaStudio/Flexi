e24cd604c214f9f514759b454a0953eea425f754
Wen Tai-Chi <daleth90@gmail.com>
2022-09-18 03:09:47 +0800

    refactor: use NodeFactory instead of constructor

--- Tests/Editor/NodeConnectionTests.cs
+++ Tests/Editor/NodeConnectionTests.cs
@@ -6,26 +6,26 @@ namespace Physalia.AbilitySystem.Tests
     {
         public class TestNode1 : Node
         {
-            public Inport<int> input = new();
+            public Inport<int> input;
         }
 
         public class TestNode2 : Node
         {
-            public Outport<int> output = new();
+            public Outport<int> output;
         }
 
         [Test]
         public void GetValue_FromEmptyInport()
         {
-            TestNode1 node1 = new TestNode1();
+            TestNode1 node1 = NodeFactory.Create<TestNode1>();
             Assert.AreEqual(0, node1.input.GetValue());
         }
 
         [Test]
         public void GetValue_FromConnectedInport()
         {
-            TestNode1 node1 = new TestNode1();
-            TestNode2 node2 = new TestNode2();
+            TestNode1 node1 = NodeFactory.Create<TestNode1>();
+            TestNode2 node2 = NodeFactory.Create<TestNode2>();
 
             node1.input.ConnectTo(node2.output);
             node2.output.SetValue(5421);
