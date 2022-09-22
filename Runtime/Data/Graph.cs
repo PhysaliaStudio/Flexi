300232d756cc11d8b967e02adaa3750236e2d3ef
Wen Tai-Chi <daleth90@gmail.com>
2022-09-22 12:19:24 +0800

    feat: add/remove nodes with Graph API
    For handling entry nodes

--- Runtime/Data/Graph.cs
+++ Runtime/Data/Graph.cs
@@ -1,12 +1,16 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using UnityEngine;
 
 namespace Physalia.AbilitySystem
 {
     [JsonConverter(typeof(GrpahConverter))]
     public class Graph
     {
+        private readonly List<EntryNode> entryNodes = new();
         internal List<Node> nodes = new();
+
+        public IReadOnlyList<EntryNode> EntryNodes => entryNodes;
 
         public void ReorderNodes()
         {
@@ -14,6 +18,36 @@ namespace Physalia.AbilitySystem
             {
                 nodes[i].id = i + 1;
             }
+        }
+
+        public T AddNode<T>() where T : Node, new()
+        {
+            T node = NodeFactory.Create<T>();
+            nodes.Add(node);
+            if (node is EntryNode entryNode)
+            {
+                entryNodes.Add(entryNode);
+            }
+
+            return node;
+        }
+
+        public void RemoveNode(Node node)
+        {
+            bool success = nodes.Remove(node);
+            if (success)
+            {
+                if (node is EntryNode entryNode)
+                {
+                    entryNodes.Remove(entryNode);
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"[{nameof(Graph)}] Try to delete node, but the node doesn't belong to this graph");
+            }
+
+            return;
         }
 
         public Node GetNode(int id)
