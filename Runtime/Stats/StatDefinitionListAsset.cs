2d5bf6d2e01a560843396f10eb28a014a930226d
Wen Tai-Chi <daleth90@gmail.com>
2022-09-27 21:25:52 +0800

    feat: add StatDefinitionListAsset to save stats

---
+++ Runtime/Stats/StatDefinitionListAsset.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Physalia.AbilitySystem
+{
+    [CreateAssetMenu(fileName = "NewStatDefinitionListAsset", menuName = "Physalia/Ability System/Stat List Asset", order = 0)]
+    public class StatDefinitionListAsset : ScriptableObject
+    {
+        public List<StatDefinition> stats = new();
+
+        /// <remarks>
+        /// The file should be the exported file, or match the format
+        /// </remarks>
+        public static StatDefinitionListAsset CreateWithFile(string fileContents)
+        {
+            string[] lines = fileContents.Replace("\r\n", "\n").Split('\n');
+            var stats = new List<StatDefinition>();
+            for (var i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] == "")
+                {
+                    continue;
+                }
+
+                StatDefinition definition = JsonUtility.FromJson<StatDefinition>(lines[i]);
+                if (definition != null)
+                {
+                    stats.Add(definition);
+                }
+                else
+                {
+                    Debug.LogError($"Parse line {i} failed!");
+                }
+            }
+
+            var instance = CreateInstance<StatDefinitionListAsset>();
+            instance.stats = stats;
+            return instance;
+        }
+
+        public static StatDefinitionListAsset CreateWithList(List<StatDefinition> stats)
+        {
+            var instance = CreateInstance<StatDefinitionListAsset>();
+            instance.stats = stats;
+            return instance;
+        }
+    }
+}
