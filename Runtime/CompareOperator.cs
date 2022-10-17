9a734a343b323fc8dec1a29b0ed4c8a831cb2bd3
Wen Tai-Chi <daleth90@gmail.com>
2022-10-17 08:19:18 +0800

    feat: add compare operator enum

---
+++ Runtime/CompareOperator.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Physalia.AbilitySystem
+{
+    public enum CompareOperator
+    {
+        EQUAL,
+        NOT_EQUAL,
+        LESS,
+        LESS_OR_EQUAL,
+        GREATER,
+        GREATER_OR_EQUAL,
+    }
+
+    public static class CompareOperatorExtensions
+    {
+        public static bool Compare<T>(this CompareOperator op, T a, T b) where T : IComparable
+        {
+            int result = a.CompareTo(b);
+            return op switch
+            {
+                CompareOperator.EQUAL => result == 0,
+                CompareOperator.NOT_EQUAL => result != 0,
+                CompareOperator.LESS => result < 0,
+                CompareOperator.LESS_OR_EQUAL => result <= 0,
+                CompareOperator.GREATER => result > 0,
+                CompareOperator.GREATER_OR_EQUAL => result >= 0,
+                _ => throw new NotImplementedException(),
+            };
+        }
+    }
+}
