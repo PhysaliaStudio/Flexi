bf96cf54fbc2438f0136b946acadcdd7f42a8639
Wen Tai-Chi <daleth90@gmail.com>
2022-10-19 21:22:13 +0800

    feat: custom stat list inspector

---
+++ Editor/StatDefinition/StatDefinitionListAssetInspector.cs
@@ -0,0 +1,63 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+namespace Physalia.AbilitySystem
+{
+    [CustomEditor(typeof(StatDefinitionListAsset))]
+    public class StatDefinitionListAssetInspector : Editor
+    {
+        [SerializeField]
+        private VisualTreeAsset editorAsset = null;
+        [SerializeField]
+        private VisualTreeAsset itemAsset = null;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            VisualElement rootVisualElement = new VisualElement();
+            if (editorAsset == null)
+            {
+                Debug.LogError($"[{nameof(StatDefinitionListAssetInspector)}] Missing VisualTreeAsset, set the corrent reference in {nameof(StatDefinitionListAssetInspector)} ScriptAsset might fix this");
+                return rootVisualElement;
+            }
+
+            if (itemAsset == null)
+            {
+                Debug.LogError($"[{nameof(StatDefinitionListAssetInspector)}] Missing VisualTreeAsset, set the corrent reference in {nameof(StatDefinitionListAssetInspector)} ScriptAsset might fix this");
+                return rootVisualElement;
+            }
+
+            editorAsset.CloneTree(rootVisualElement);
+
+            var listView = rootVisualElement.Q<ListView>();
+            SerializedProperty listProperty = serializedObject.FindProperty(nameof(StatDefinitionListAsset.stats));
+            listView.BindProperty(listProperty);
+            listView.makeItem = itemAsset.CloneTree;
+            listView.bindItem = BindItem;
+            listView.unbindItem = UnbindItem;
+
+            return rootVisualElement;
+        }
+
+        private void BindItem(VisualElement element, int i)
+        {
+            SerializedProperty listProperty = serializedObject.FindProperty(nameof(StatDefinitionListAsset.stats));
+            var idField = element.Q<IntegerField>("id-field");
+            var nameField = element.Q<TextField>("name-field");
+
+            SerializedProperty property = listProperty.GetArrayElementAtIndex(i);
+            idField.BindProperty(property.FindPropertyRelative(nameof(StatDefinition.Id)));
+            nameField.BindProperty(property.FindPropertyRelative(nameof(StatDefinition.Name)));
+        }
+
+        private void UnbindItem(VisualElement element, int i)
+        {
+            var idField = element.Q<IntegerField>(name: "id-field");
+            var nameField = element.Q<TextField>(name: "name-field");
+
+            idField.Unbind();
+            nameField.Unbind();
+        }
+    }
+}
