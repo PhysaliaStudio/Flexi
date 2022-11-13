b85ce41ec52bf7ded536eb0a3a51a5e4cdecff60
Wen Tai-Chi <daleth90@gmail.com>
2022-11-13 23:34:47 +0800

    feat: add for loop node

---
+++ Runtime/BuiltIn/Nodes/ForLoopNode.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+
+namespace Physalia.AbilityFramework
+{
+    public class ForLoopNode : FlowNode
+    {
+        internal Inport<FlowNode> previousPort;
+        internal Inport<int> startIndexPort;
+        internal Inport<int> endIndexPort;
+
+        internal Outport<FlowNode> loopBodyPort;
+        internal Outport<int> indexPort;
+        internal Outport<FlowNode> completedPort;
+
+        private bool hasStarted;
+        private bool isIncrement;
+        private int index;
+
+        public override FlowNode Previous
+        {
+            get
+            {
+                IReadOnlyList<Port> connections = previousPort.GetConnections();
+                return connections.Count > 0 ? connections[0].Node as FlowNode : null;
+            }
+        }
+
+        public override FlowNode Next
+        {
+            get
+            {
+                IReadOnlyList<Port> connections;
+                if (isIncrement && index < endIndexPort.GetValue())
+                {
+                    connections = loopBodyPort.GetConnections();
+                }
+                else if (!isIncrement && index >= endIndexPort.GetValue())
+                {
+                    connections = loopBodyPort.GetConnections();
+                }
+                else
+                {
+                    connections = completedPort.GetConnections();
+                }
+
+                return connections.Count > 0 ? connections[0].Node as FlowNode : null;
+            }
+        }
+
+        protected override AbilityState DoLogic()
+        {
+            if (!hasStarted)
+            {
+                hasStarted = true;
+                int start = startIndexPort.GetValue();
+                int end = endIndexPort.GetValue();
+
+                isIncrement = end >= start;
+                index = isIncrement ? start : start - 1;
+            }
+            else if (isIncrement && index < endIndexPort.GetValue())
+            {
+                index++;
+            }
+            else if (!isIncrement && index >= endIndexPort.GetValue())
+            {
+                index--;
+            }
+
+            indexPort.SetValue(index);
+            return AbilityState.RUNNING;
+        }
+
+        protected internal override void Reset()
+        {
+            hasStarted = false;
+            isIncrement = false;
+            index = 0;
+        }
+    }
+}
---
+++ Tests/Editor/Nodes/ForLoopNodeTests.cs
@@ -0,0 +1,174 @@
+using NUnit.Framework;
+
+namespace Physalia.AbilityFramework.Tests
+{
+    public class ForLoopNodeTests
+    {
+        [Test]
+        public void IncrementLoop()
+        {
+            var forLoopNode = NodeFactory.Create<ForLoopNode>();
+            var startIntNode = NodeFactory.Create<IntegerNode>();
+            var endIntNode = NodeFactory.Create<IntegerNode>();
+            var loopBodyNode = NodeFactory.Create<RelayNode>();
+            var completedNode = NodeFactory.Create<RelayNode>();
+
+            forLoopNode.startIndexPort.Connect(startIntNode.output);
+            forLoopNode.endIndexPort.Connect(endIntNode.output);
+            forLoopNode.loopBodyPort.Connect(loopBodyNode.previous);
+            forLoopNode.completedPort.Connect(completedNode.previous);
+
+            startIntNode.value.Value = 0;
+            endIntNode.value.Value = 10;
+
+            // In loop
+            for (var i = 0; i < 10; i++)
+            {
+                forLoopNode.Run();
+                Assert.AreEqual(loopBodyNode, forLoopNode.Next, "Expected to go to loop body but failed");
+                Assert.AreEqual(i, forLoopNode.indexPort.GetValue());
+            }
+
+            // Out loop
+            forLoopNode.Run();
+            Assert.AreEqual(completedNode, forLoopNode.Next, "Expected to go to completed but failed");
+            Assert.AreEqual(10, forLoopNode.indexPort.GetValue());
+        }
+
+        [Test]
+        public void DecrementLoop()
+        {
+            var forLoopNode = NodeFactory.Create<ForLoopNode>();
+            var startIntNode = NodeFactory.Create<IntegerNode>();
+            var endIntNode = NodeFactory.Create<IntegerNode>();
+            var loopBodyNode = NodeFactory.Create<RelayNode>();
+            var completedNode = NodeFactory.Create<RelayNode>();
+
+            forLoopNode.startIndexPort.Connect(startIntNode.output);
+            forLoopNode.endIndexPort.Connect(endIntNode.output);
+            forLoopNode.loopBodyPort.Connect(loopBodyNode.previous);
+            forLoopNode.completedPort.Connect(completedNode.previous);
+
+            startIntNode.value.Value = 10;
+            endIntNode.value.Value = 0;
+
+            // In loop
+            for (var i = 9; i >= 0; i--)
+            {
+                forLoopNode.Run();
+                Assert.AreEqual(loopBodyNode, forLoopNode.Next, "Expected to go to loop body but failed");
+                Assert.AreEqual(i, forLoopNode.indexPort.GetValue());
+            }
+
+            // Out loop
+            forLoopNode.Run();
+            Assert.AreEqual(completedNode, forLoopNode.Next, "Expected to go to completed but failed");
+            Assert.AreEqual(-1, forLoopNode.indexPort.GetValue());
+        }
+
+        [Test]
+        public void StartIndexEqualsEndIndex_NoLoop()
+        {
+            var forLoopNode = NodeFactory.Create<ForLoopNode>();
+            var startIntNode = NodeFactory.Create<IntegerNode>();
+            var endIntNode = NodeFactory.Create<IntegerNode>();
+            var loopBodyNode = NodeFactory.Create<RelayNode>();
+            var completedNode = NodeFactory.Create<RelayNode>();
+
+            forLoopNode.startIndexPort.Connect(startIntNode.output);
+            forLoopNode.endIndexPort.Connect(endIntNode.output);
+            forLoopNode.loopBodyPort.Connect(loopBodyNode.previous);
+            forLoopNode.completedPort.Connect(completedNode.previous);
+
+            startIntNode.value.Value = 5;
+            endIntNode.value.Value = 5;
+
+            // Out loop
+            forLoopNode.Run();
+            Assert.AreEqual(completedNode, forLoopNode.Next, "Expected to go to completed but failed");
+            Assert.AreEqual(5, forLoopNode.indexPort.GetValue());  // The index does not increment because the loop never run
+        }
+
+        [Test]
+        public void RunAfterLoopIsFinish_TheIndexDoesNotChange()
+        {
+            var forLoopNode = NodeFactory.Create<ForLoopNode>();
+            var startIntNode = NodeFactory.Create<IntegerNode>();
+            var endIntNode = NodeFactory.Create<IntegerNode>();
+            var loopBodyNode = NodeFactory.Create<RelayNode>();
+            var completedNode = NodeFactory.Create<RelayNode>();
+
+            forLoopNode.startIndexPort.Connect(startIntNode.output);
+            forLoopNode.endIndexPort.Connect(endIntNode.output);
+            forLoopNode.loopBodyPort.Connect(loopBodyNode.previous);
+            forLoopNode.completedPort.Connect(completedNode.previous);
+
+            startIntNode.value.Value = 0;
+            endIntNode.value.Value = 10;
+
+            // In loop
+            for (var i = 0; i < 10; i++)
+            {
+                forLoopNode.Run();
+                Assert.AreEqual(loopBodyNode, forLoopNode.Next, "Expected to go to loop body but failed");
+                Assert.AreEqual(i, forLoopNode.indexPort.GetValue());
+            }
+
+            // Out loop
+            forLoopNode.Run();
+            Assert.AreEqual(completedNode, forLoopNode.Next, "Expected to go to completed but failed");
+            Assert.AreEqual(10, forLoopNode.indexPort.GetValue());
+
+            // Run Again
+            forLoopNode.Run();
+            Assert.AreEqual(completedNode, forLoopNode.Next, "Expected to go to completed but failed");
+            Assert.AreEqual(10, forLoopNode.indexPort.GetValue());
+        }
+
+        [Test]
+        public void Reset_TheLoopRestarts()
+        {
+            var forLoopNode = NodeFactory.Create<ForLoopNode>();
+            var startIntNode = NodeFactory.Create<IntegerNode>();
+            var endIntNode = NodeFactory.Create<IntegerNode>();
+            var loopBodyNode = NodeFactory.Create<RelayNode>();
+            var completedNode = NodeFactory.Create<RelayNode>();
+
+            forLoopNode.startIndexPort.Connect(startIntNode.output);
+            forLoopNode.endIndexPort.Connect(endIntNode.output);
+            forLoopNode.loopBodyPort.Connect(loopBodyNode.previous);
+            forLoopNode.completedPort.Connect(completedNode.previous);
+
+            startIntNode.value.Value = 0;
+            endIntNode.value.Value = 10;
+
+            // In loop
+            for (var i = 0; i < 10; i++)
+            {
+                forLoopNode.Run();
+                Assert.AreEqual(loopBodyNode, forLoopNode.Next, "Expected to go to loop body but failed");
+                Assert.AreEqual(i, forLoopNode.indexPort.GetValue());
+            }
+
+            // Out loop
+            forLoopNode.Run();
+            Assert.AreEqual(completedNode, forLoopNode.Next, "Expected to go to completed but failed");
+            Assert.AreEqual(10, forLoopNode.indexPort.GetValue());
+
+            forLoopNode.Reset();
+
+            // In loop
+            for (var i = 0; i < 10; i++)
+            {
+                forLoopNode.Run();
+                Assert.AreEqual(loopBodyNode, forLoopNode.Next, "Expected to go to loop body but failed");
+                Assert.AreEqual(i, forLoopNode.indexPort.GetValue());
+            }
+
+            // Out loop
+            forLoopNode.Run();
+            Assert.AreEqual(completedNode, forLoopNode.Next, "Expected to go to completed but failed");
+            Assert.AreEqual(10, forLoopNode.indexPort.GetValue());
+        }
+    }
+}
