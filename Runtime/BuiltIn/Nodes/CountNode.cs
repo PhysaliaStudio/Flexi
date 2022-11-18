11a3d0e0e7100d93cbdf99b3b1302fcd22e74d0e
Wen Tai-Chi <daleth90@gmail.com>
2022-11-18 14:24:43 +0800

    feat: add count node to count collection

---
+++ Runtime/BuiltIn/Nodes/CountNode.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+
+namespace Physalia.AbilityFramework
+{
+    public class CountNode : ValueNode
+    {
+        public Inport<ICollection> collectionPort;
+        public Outport<int> countPort;
+
+        protected override void EvaluateSelf()
+        {
+            ICollection collection = collectionPort.GetValue();
+            if (collection == null)
+            {
+                countPort.SetValue(0);
+                return;
+            }
+
+            countPort.SetValue(collection.Count);
+        }
+    }
+}
