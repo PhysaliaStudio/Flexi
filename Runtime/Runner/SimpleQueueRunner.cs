aa93ddde8737e780c06ebfe408227510e5878356
Wen Tai-Chi <daleth90@gmail.com>
2022-12-28 05:26:39 +0800

    feat: implement a simple queue runner
    For running stat refresh flows individually

---
+++ Runtime/Runner/SimpleQueueRunner.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+
+namespace Physalia.AbilityFramework
+{
+    public sealed class SimpleQueueRunner : AbilityRunner
+    {
+        private readonly Queue<IAbilityFlow> queue = new();
+
+        public override IAbilityFlow Peek()
+        {
+            bool success = queue.TryPeek(out IAbilityFlow flow);
+            if (success)
+            {
+                return flow;
+            }
+
+            return null;
+        }
+
+        public override void EnqueueFlow(IAbilityFlow flow)
+        {
+            queue.Enqueue(flow);
+        }
+
+        internal override IAbilityFlow DequeueFlow()
+        {
+            bool success = queue.TryDequeue(out IAbilityFlow flow);
+            if (success)
+            {
+                return flow;
+            }
+
+            return null;
+        }
+
+        public override void AddNewQueue()
+        {
+
+        }
+
+        public override void RemoveEmptyQueues()
+        {
+
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+            queue.Clear();
+        }
+    }
+}
