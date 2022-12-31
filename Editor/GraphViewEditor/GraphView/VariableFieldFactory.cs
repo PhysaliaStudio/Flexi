1f0491f99c1e7a659351cf8a985bd50f52dcedf1
Wen Tai-Chi <daleth90@gmail.com>
2023-01-01 04:33:40 +0800

    feat: support more built-in types for variable fields

--- Editor/GraphViewEditor/GraphView/VariableFieldFactory.cs
+++ Editor/GraphViewEditor/GraphView/VariableFieldFactory.cs
@@ -21,16 +21,94 @@ namespace Physalia.Flexi.GraphViewEditor
                     {
                         return CreateVariableFieldWithProperty(label, variable, window);
                     }
+                case Variable<string> stringVariable:
+                    {
+                        var field = new TextField(label);
+                        SetUpField(field, stringVariable, window);
+                        return field;
+                    }
                 case Variable<int> intVariable:
                     {
                         var field = new IntegerField(label);
                         SetUpField(field, intVariable, window);
                         return field;
                     }
-                case Variable<string> stringVariable:
+                case Variable<long> longVariable:
                     {
-                        var field = new TextField(label);
-                        SetUpField(field, stringVariable, window);
+                        var field = new LongField(label);
+                        SetUpField(field, longVariable, window);
+                        return field;
+                    }
+                case Variable<float> floatVariable:
+                    {
+                        var field = new FloatField(label);
+                        SetUpField(field, floatVariable, window);
+                        return field;
+                    }
+                case Variable<double> doubleVariable:
+                    {
+                        var field = new DoubleField(label);
+                        SetUpField(field, doubleVariable, window);
+                        return field;
+                    }
+                case Variable<Vector2> vector2Variable:
+                    {
+                        var field = new Vector2Field(label);
+                        SetUpField(field, vector2Variable, window);
+                        return field;
+                    }
+                case Variable<Vector3> vector3Variable:
+                    {
+                        var field = new Vector3Field(label);
+                        SetUpField(field, vector3Variable, window);
+                        return field;
+                    }
+                case Variable<Vector4> vector4Variable:
+                    {
+                        var field = new Vector4Field(label);
+                        SetUpField(field, vector4Variable, window);
+                        return field;
+                    }
+                case Variable<Vector2Int> vector2IntVariable:
+                    {
+                        var field = new Vector2IntField(label);
+                        SetUpField(field, vector2IntVariable, window);
+                        return field;
+                    }
+                case Variable<Vector3Int> vector3IntVariable:
+                    {
+                        var field = new Vector3IntField(label);
+                        SetUpField(field, vector3IntVariable, window);
+                        return field;
+                    }
+                case Variable<Color> colorVariable:
+                    {
+                        var field = new ColorField(label);
+                        SetUpField(field, colorVariable, window);
+                        return field;
+                    }
+                case Variable<Rect> rectVariable:
+                    {
+                        var field = new RectField(label);
+                        SetUpField(field, rectVariable, window);
+                        return field;
+                    }
+                case Variable<RectInt> rectIntVariable:
+                    {
+                        var field = new RectIntField(label);
+                        SetUpField(field, rectIntVariable, window);
+                        return field;
+                    }
+                case Variable<Bounds> boundsVariable:
+                    {
+                        var field = new BoundsField(label);
+                        SetUpField(field, boundsVariable, window);
+                        return field;
+                    }
+                case Variable<BoundsInt> boundsIntVariable:
+                    {
+                        var field = new BoundsIntField(label);
+                        SetUpField(field, boundsIntVariable, window);
                         return field;
                     }
             }
