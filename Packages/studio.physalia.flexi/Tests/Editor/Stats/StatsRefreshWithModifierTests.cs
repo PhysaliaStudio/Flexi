c72f11ea2496506bf0b448821335040e2d2da488
Wen Taichi <daleth90@gmail.com>
2023-05-23 17:58:52 +0800

    feat: replace StatOwner with Actor to test StatsRefresh

--- Packages/studio.physalia.flexi/Tests/Editor/Stats/StatsRefreshWithModifierTests.cs
+++ Packages/studio.physalia.flexi/Tests/Editor/Stats/StatsRefreshWithModifierTests.cs
@@ -4,42 +4,46 @@ namespace Physalia.Flexi.Tests
 {
     public class StatsRefreshWithModifierTests
     {
-        private StatOwnerRepository repository;
+        private AbilitySystem abilitySystem;
 
         [SetUp]
         public void SetUp()
         {
-            StatDefinitionListAsset statDefinitionList = StatDefinitionListAsset.CreateWithList(StatTestHelper.ValidList);
-            repository = StatOwnerRepository.Create(statDefinitionList);
+            AbilitySystemBuilder builder = new AbilitySystemBuilder();
+
+            StatDefinitionListAsset statDefinitionListAsset = StatDefinitionListAsset.CreateWithList(StatTestHelper.ValidList);
+            builder.SetStatDefinitions(statDefinitionListAsset);
+
+            abilitySystem = builder.Build();
         }
 
-        private StatOwner CreateOwner()
+        private Actor CreateActor()
         {
-            StatOwner owner = repository.CreateOwner();
-            owner.AddStat(StatTestHelper.HEALTH, 100);
-            owner.AddStat(StatTestHelper.MAX_HEALTH, 100);
-            owner.AddStat(StatTestHelper.ATTACK, 12);
-            return owner;
+            var actor = new EmptyActor(abilitySystem);
+            actor.AddStat(StatTestHelper.HEALTH, 100);
+            actor.AddStat(StatTestHelper.MAX_HEALTH, 100);
+            actor.AddStat(StatTestHelper.ATTACK, 12);
+            return actor;
         }
 
         [Test]
         public void SingleModifier_WithStatIdOwnerNotOwned_NoError()
         {
-            var owner = CreateOwner();
-            owner.RemoveStat(StatTestHelper.ATTACK);
-            owner.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 50, StatModifier.Operator.MUL));
-            repository.RefreshStats(owner);
+            var actor = CreateActor();
+            actor.RemoveStat(StatTestHelper.ATTACK);
+            actor.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 50, StatModifier.Operator.MUL));
+            actor.RefreshStats();
 
-            Assert.IsNull(owner.GetStat(StatTestHelper.ATTACK));
+            Assert.IsNull(actor.GetStat(StatTestHelper.ATTACK));
             Assert.Pass();
         }
 
         [Test]
         public void SingleModifier_WithInvalidStatId_NoError()
         {
-            var owner = CreateOwner();
-            owner.AppendModifier(new StatModifier(999, 50, StatModifier.Operator.MUL));
-            repository.RefreshStats(owner);
+            var actor = CreateActor();
+            actor.AppendModifier(new StatModifier(999, 50, StatModifier.Operator.MUL));
+            actor.RefreshStats();
 
             Assert.Pass();
         }
@@ -47,66 +51,66 @@ namespace Physalia.Flexi.Tests
         [Test]
         public void SingleModifier_100Minus10_CurrentBaseReturns100AndCurrentValueReturns90()
         {
-            var owner = CreateOwner();
-            owner.AppendModifier(new StatModifier(StatTestHelper.MAX_HEALTH, -10, StatModifier.Operator.ADD));
-            repository.RefreshStats(owner);
+            var actor = CreateActor();
+            actor.AppendModifier(new StatModifier(StatTestHelper.MAX_HEALTH, -10, StatModifier.Operator.ADD));
+            actor.RefreshStats();
 
-            Assert.AreEqual(100, owner.GetStat(StatTestHelper.MAX_HEALTH).CurrentBase);
-            Assert.AreEqual(90, owner.GetStat(StatTestHelper.MAX_HEALTH).CurrentValue);
+            Assert.AreEqual(100, actor.GetStat(StatTestHelper.MAX_HEALTH).CurrentBase);
+            Assert.AreEqual(90, actor.GetStat(StatTestHelper.MAX_HEALTH).CurrentValue);
         }
 
         [Test]
         public void SingleModifier_12Mul50Percent_CurrentBaseReturns12AndCurrentValueReturns18()
         {
-            var owner = CreateOwner();
-            owner.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 50, StatModifier.Operator.MUL));
-            repository.RefreshStats(owner);
+            var actor = CreateActor();
+            actor.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 50, StatModifier.Operator.MUL));
+            actor.RefreshStats();
 
-            Assert.AreEqual(12, owner.GetStat(StatTestHelper.ATTACK).CurrentBase);
-            Assert.AreEqual(18, owner.GetStat(StatTestHelper.ATTACK).CurrentValue);
+            Assert.AreEqual(12, actor.GetStat(StatTestHelper.ATTACK).CurrentBase);
+            Assert.AreEqual(18, actor.GetStat(StatTestHelper.ATTACK).CurrentValue);
         }
 
         [Test]
         public void MultipleModifier_Base12Add6AndMul50Percent_CurrentBaseReturns12AndCurrentValueReturns27()
         {
-            var owner = CreateOwner();
-            owner.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 50, StatModifier.Operator.MUL));
-            owner.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 6, StatModifier.Operator.ADD));
-            repository.RefreshStats(owner);
+            var actor = CreateActor();
+            actor.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 50, StatModifier.Operator.MUL));
+            actor.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 6, StatModifier.Operator.ADD));
+            actor.RefreshStats();
 
-            Assert.AreEqual(12, owner.GetStat(StatTestHelper.ATTACK).CurrentBase);
-            Assert.AreEqual(27, owner.GetStat(StatTestHelper.ATTACK).CurrentValue);
+            Assert.AreEqual(12, actor.GetStat(StatTestHelper.ATTACK).CurrentBase);
+            Assert.AreEqual(27, actor.GetStat(StatTestHelper.ATTACK).CurrentValue);
         }
 
         [Test]
         public void MultipleModifier_WithDifferentStatIds()
         {
-            var owner = CreateOwner();
-            owner.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 50, StatModifier.Operator.MUL));
-            owner.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 6, StatModifier.Operator.ADD));
-            owner.AppendModifier(new StatModifier(StatTestHelper.MAX_HEALTH, -10, StatModifier.Operator.ADD));
-            repository.RefreshStats(owner);
+            var actor = CreateActor();
+            actor.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 50, StatModifier.Operator.MUL));
+            actor.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 6, StatModifier.Operator.ADD));
+            actor.AppendModifier(new StatModifier(StatTestHelper.MAX_HEALTH, -10, StatModifier.Operator.ADD));
+            actor.RefreshStats();
 
-            Assert.AreEqual(100, owner.GetStat(StatTestHelper.MAX_HEALTH).CurrentBase);
-            Assert.AreEqual(90, owner.GetStat(StatTestHelper.MAX_HEALTH).CurrentValue);
-            Assert.AreEqual(12, owner.GetStat(StatTestHelper.ATTACK).CurrentBase);
-            Assert.AreEqual(27, owner.GetStat(StatTestHelper.ATTACK).CurrentValue);
+            Assert.AreEqual(100, actor.GetStat(StatTestHelper.MAX_HEALTH).CurrentBase);
+            Assert.AreEqual(90, actor.GetStat(StatTestHelper.MAX_HEALTH).CurrentValue);
+            Assert.AreEqual(12, actor.GetStat(StatTestHelper.ATTACK).CurrentBase);
+            Assert.AreEqual(27, actor.GetStat(StatTestHelper.ATTACK).CurrentValue);
         }
 
         [Test]
         public void RefreshTwice_ReturnsTheSameValues()
         {
-            var owner = CreateOwner();
-            owner.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 50, StatModifier.Operator.MUL));
-            owner.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 6, StatModifier.Operator.ADD));
-            owner.AppendModifier(new StatModifier(StatTestHelper.MAX_HEALTH, -10, StatModifier.Operator.ADD));
-            repository.RefreshStats(owner);
-            repository.RefreshStats(owner);
+            var actor = CreateActor();
+            actor.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 50, StatModifier.Operator.MUL));
+            actor.AppendModifier(new StatModifier(StatTestHelper.ATTACK, 6, StatModifier.Operator.ADD));
+            actor.AppendModifier(new StatModifier(StatTestHelper.MAX_HEALTH, -10, StatModifier.Operator.ADD));
+            actor.RefreshStats();
+            actor.RefreshStats();
 
-            Assert.AreEqual(100, owner.GetStat(StatTestHelper.MAX_HEALTH).CurrentBase);
-            Assert.AreEqual(90, owner.GetStat(StatTestHelper.MAX_HEALTH).CurrentValue);
-            Assert.AreEqual(12, owner.GetStat(StatTestHelper.ATTACK).CurrentBase);
-            Assert.AreEqual(27, owner.GetStat(StatTestHelper.ATTACK).CurrentValue);
+            Assert.AreEqual(100, actor.GetStat(StatTestHelper.MAX_HEALTH).CurrentBase);
+            Assert.AreEqual(90, actor.GetStat(StatTestHelper.MAX_HEALTH).CurrentValue);
+            Assert.AreEqual(12, actor.GetStat(StatTestHelper.ATTACK).CurrentBase);
+            Assert.AreEqual(27, actor.GetStat(StatTestHelper.ATTACK).CurrentValue);
         }
     }
 }
