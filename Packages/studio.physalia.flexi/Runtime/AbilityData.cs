a6578a9c1537ece6a952b4f99789c08ec61da6ce
Wen Taichi <daleth90@gmail.com>
2023-06-06 21:16:50 +0800

    feat: able to override blackboard in AbilityData

--- Packages/studio.physalia.flexi/Runtime/AbilityData.cs
+++ Packages/studio.physalia.flexi/Runtime/AbilityData.cs
@@ -12,5 +12,22 @@ namespace Physalia.Flexi
         public string name;
         public List<BlackboardVariable> blackboard = new();
         public List<string> graphJsons = new();
+
+        /// <remarks>
+        /// This method is for overriding values from outside data like Excel.
+        /// </remarks>
+        public void SetBlackboard(string key, int value)
+        {
+            for (var i = 0; i < blackboard.Count; i++)
+            {
+                if (blackboard[i].key == key)
+                {
+                    blackboard[i].value = value;
+                    return;
+                }
+            }
+
+            blackboard.Add(new BlackboardVariable { key = key, value = value });
+        }
     }
 }
