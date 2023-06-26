6e017035703a89297d886e49f2af760d89b559a3
Wen Taichi <daleth90@gmail.com>
2023-06-26 10:59:43 +0800

    feat: add AbilityDataSource

---
+++ Packages/studio.physalia.flexi/Runtime/AbilityDataSource.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+
+namespace Physalia.Flexi
+{
+    public readonly struct AbilityDataSource : IEqualityComparer<AbilityDataSource>
+    {
+        private readonly AbilityData abilityData;
+        private readonly int groupIndex;
+
+        public readonly AbilityData AbilityData => abilityData;
+        public readonly int GroupIndex => groupIndex;
+
+        public AbilityDataSource(AbilityData abilityData, int groupIndex)
+        {
+            this.abilityData = abilityData;
+            this.groupIndex = groupIndex;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is AbilityDataSource other && Equals(this, other);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetHashCode(this);
+        }
+
+        public bool Equals(AbilityDataSource x, AbilityDataSource y)
+        {
+            return x.AbilityData == y.AbilityData && x.GroupIndex == y.GroupIndex;
+        }
+
+        public int GetHashCode(AbilityDataSource obj)
+        {
+            return obj.AbilityData.GetHashCode() ^ obj.GroupIndex.GetHashCode();
+        }
+
+        public static bool operator ==(AbilityDataSource left, AbilityDataSource right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(AbilityDataSource left, AbilityDataSource right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
