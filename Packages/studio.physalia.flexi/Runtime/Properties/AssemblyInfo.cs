bfe4bf69e80f5bacfb953dc7828fb0d403d34bdf
Wen Taichi <daleth90@gmail.com>
2023-09-04 04:13:36 +0800

    feat: implement node searcher

--- Packages/studio.physalia.flexi/Runtime/Properties/AssemblyInfo.cs
+++ Packages/studio.physalia.flexi/Runtime/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Physalia.Flexi.Editor.Tests")]
 [assembly: InternalsVisibleTo("Physalia.Flexi.GraphViewEditor")]
 [assembly: InternalsVisibleTo("Physalia.Flexi.GraphDataFixer")]
+[assembly: InternalsVisibleTo("Physalia.Flexi.NodeSearcher")]
